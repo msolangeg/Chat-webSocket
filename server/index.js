cd7e360841c88cbe52b7964a81bccc1ee9529ae8
masolangeg <marina.solange.g@gmail.com>
2024-04-03 20:23:41 -0300

    connection server

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT ?? 3001
 
 const app= express()
 const server = createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    connectionStateRecovery : {}
+})
 
 io.on('connection', (socket) => {
     console.log('a user has connected!')
@@ -17,6 +19,12 @@ io.on('connection', (socket) => {
     socket.on('disconnet', () => {
         console.log('a user has connected!')
     })
+
+ // comunicación del cliente al servidor
+    socket.on('chat message', (msg) => {
+        // console.log('message: ' + msg)
+        io.emit('chat message', msg)
+    })  
 })
 
 app.use(logger('dev'))
